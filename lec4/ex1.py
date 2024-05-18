db96c5e5151c369931b2dff8fd8e3d12cf9a62b6
Isaac Skog <isaac.skog@icloud.com>
2024-05-18 14:42:37 +0200

    use try and except method

--- lec4/ex1.py
+++ lec4/ex1.py
@@ -1,18 +1,8 @@
-#övning använd try except
 
-def main():
+my_list=[1,2,3,4,5]
 
-    # min lista
-    my_list=[1,2,3,4,5]
-
-    # visa lista 
-    print(f"The list is: {my_list}")
-
-    # While
-    while True:
-
+while True:
         try:
-
             # Skapa en variabel 
             index = int(input("Write index: "))
             if index < 0 or index >= len(my_list):
@@ -23,8 +13,3 @@ def main():
 
         except ValueError:
             print("Error: write a valid integer")
-
-if __name__ == "__main__":
-    main()
-
-
