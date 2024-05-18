cd969d16c39b2b80e47754a8885fe64f1adaaf54
Isaac Skog <isaac.skog@icloud.com>
2024-05-18 15:52:02 +0200

    Add tests

---
+++ pytest/test_program.py
@@ -0,0 +1,10 @@
+from program import *
+
+def test_area_of_square():
+    assert area_of_square(2)==4
+
+# def test_area_of_rectangle():
+#     assert area_of_rectangle(2,3)==6
+
+def test_add():
+    assert add(2,2)==4
