fced3f5c1f6e3a47146c6a930409c5125596f30c
Isaac Skog <isaac.skog@icloud.com>
2024-05-20 12:50:16 +0200

    Add metod, roll the dice

--- uppgift 1/yahtzee.py
+++ uppgift 1/yahtzee.py
@@ -4,3 +4,6 @@ class YahtzeeGame:
     def __init__(self):
         self.dice = [random.randint(1,6) for _ in range(5)]
         self.roll_count=0
+
+    def roll_dice(self):
+        self.dice = [random.randint(1,6) for _ in range(5)]
