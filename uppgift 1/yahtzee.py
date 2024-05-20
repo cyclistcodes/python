1aebacbad78327cce88e24951f43fea42a6a6d87
Isaac Skog <isaac.skog@icloud.com>
2024-05-20 13:31:22 +0200

    Roll five dice and return their values

--- uppgift 1/yahtzee.py
+++ uppgift 1/yahtzee.py
@@ -1,15 +1,5 @@
-import random
+import random 
 
-class YahtzeeGame:
-    def __init__(self):
-        self.dice = [random.randint(1,6) for _ in range(5)]
-        self.roll_count=0
+def roll_dice():
+    return [random.randint(1,6) for _ in range]
 
-    def roll_dice(self):
-        self.dice = [random.randint(1,6) for _ in range(5)]
-
-    def print_dice(self):
-        print("Dice roll: ",self.dice)
-
-    def is_yahtzee(self):
-        return all(die == self.dice[0] for die in self.dice)
